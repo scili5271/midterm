3701e876578cdc5ea5a5a30521e9c729d4ca7a35
107061135 <s410067@bmsh.tn.edu.tw>
2021-04-07 01:12:16 -0700

    add main.cpp

--- midterm/main.cpp
+++ midterm/main.cpp
@@ -54,6 +54,66 @@ void sele()
     uLCD.text_height(4);
     uLCD.color(RED);
     uLCD.printf("\n%.3f\n",now);
+    if (now == 1)
+    {
+        for (int i = 0; i < 240; i++) {
+            if ( i < 80)
+            {
+                Aout = Aout + 0.01136;
+                ThisThread::sleep_for(1ms);
+            }
+            else if (i > 160)
+            {
+                Aout = Aout-0.0136;
+                ThisThread::sleep_for(1ms);
+            }
+        }
+    }
+    else if (now == 0.5)
+    {
+        for (int i = 0; i < 240; i++) {
+            if ( i < 40)
+            {
+                Aout = Aout + 0.02272;
+                ThisThread::sleep_for(1ms);
+            }
+            else if (i > 200)
+            {
+                Aout = Aout - 0.02272;
+                ThisThread::sleep_for(1ms);
+            }
+        }
+    }
+    else if (now == 0.25)
+    {
+        for (int i = 0; i < 240; i++) {
+            if ( i < 20)
+            {
+                Aout = Aout + 0.04545;
+                ThisThread::sleep_for(1ms);
+            }
+            else if (i > 220)
+            {
+                Aout = Aout - 0.04545;
+                ThisThread::sleep_for(1ms);
+            }
+        }
+    }
+    else if (now == 0.125)
+    {
+        for (int i = 0; i < 240; i++) {
+            if ( i < 10)
+            {
+                Aout = Aout + 0.0909;
+                ThisThread::sleep_for(1ms);
+            }
+            else if (i > 230)
+            {
+                Aout = Aout-0.0909;
+                ThisThread::sleep_for(1ms);
+            }
+        }
+    }
 }
 
 
